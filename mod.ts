80c7b75d3dca6a351e96a5ce73ff0cce7b7c1f86
Gnlow <gnlow@pm.me>
2025-08-20 01:19:47 +0900

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -19,8 +19,16 @@ const diff =
 const text1 = await Deno.readTextFile("./lorem.txt")
 
 console.log(tlsh(text1))
+// 1E5179716D8E03200AE3117573CAC87DF298E16416C367A5ECF7761DB14B50CA3232E9
+
 console.log(tlsh(text1.replace("in sapien", "in sapiex")))
-console.log(tlsh(text1.replace("in sapien", "in sapiev")))
+// 115179716D8E03200AE3117573CAC87DF298E16416C367A5ECF7761DB14B50CA3232E9
+
+console.log(tlsh(text1.replace("Lorem", "Orem")))
+// E25179716D8E03200AE3117573CAC87DF298E16416C367A5ECF7761DB14B50CA3232E9
+
+console.log(tlsh(text1.replaceAll("e", "i")))
+// 1E5110756CCD06251FE112BAA2999C7EE7C4A0F427C31789FCF3831AB61A618D32247C
 
 console.log(diff(
     text1,
